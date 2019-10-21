e13048f042b73bc9bafc66e0ba89727bf9b8aaa4
Viktor Lukovets <nuerio63@gmail.com>
2019-10-21 09:32:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,35 @@
 'use strict';
 
-const itemContainer = document.querySelector('#item-container');
 const previousItemBtn = document.querySelector('.carousel__btn_prev');
 const nextItemBtn = document.querySelector('.carousel__btn_next');
-let currentItem = document.querySelector('.carousel .carousel__item');
 let currentDot = document.querySelector('.carousel__dots-wrap .carousel__dot');
-
-const putItemInContainer = (item) => {
-  itemContainer.innerHTML = item.outerHTML;
-  const activeItem = document.querySelector('#item-container li');
-  activeItem.style.display = 'block';
-};
-putItemInContainer(currentItem);
+const style = document.createElement('style');
+let currentPos = 0;
 
 const nextBtnHandler = () => {
-  currentDot.className = 'carousel__dot';
-  if (!currentItem.nextElementSibling) {
-    currentDot = document.querySelector('.carousel__dots-wrap .carousel__dot');
-    currentItem = document.querySelector('.carousel .carousel__item');
-  } else {
-    currentDot = currentDot.nextElementSibling;
-    currentItem = currentItem.nextElementSibling;
+  if (!currentDot.nextElementSibling) {
+    return;
   }
+  currentDot.className = 'carousel__dot';
+  currentDot = currentDot.nextElementSibling;
   currentDot.classList.add('carousel__dot_active');
-  putItemInContainer(currentItem);
-  const animItem = document.querySelector('#item-container .twitter-card');
-  animItem.style.animation = 'slide-in 0.5s';
+  style.innerHTML = `.carousel__item {
+    right: ${currentPos += 330}px;
+  }`;
+  document.head.appendChild(style);
 };
 
 const prevBtnHandler = () => {
-  currentDot.className = 'carousel__dot';
-  if (!currentItem.previousElementSibling) {
-    currentDot = document.querySelector(
-      '.carousel__dots-wrap .carousel__dot:last-of-type'
-    );
-    currentItem = document.querySelector(
-      '.carousel .carousel__item:last-of-type'
-    );
-  } else {
-    currentDot = currentDot.previousElementSibling;
-    currentItem = currentItem.previousElementSibling;
+  if (!currentDot.previousElementSibling) {
+    return;
   }
+  currentDot.className = 'carousel__dot';
+  currentDot = currentDot.previousElementSibling;
   currentDot.classList.add('carousel__dot_active');
-  putItemInContainer(currentItem);
-  const animItem = document.querySelector('#item-container .twitter-card');
-  animItem.style.animation = 'slide-away 0.5s';
+  style.innerHTML = `.carousel__item {
+    right: ${currentPos -= 330}px;
+  }`;
+  document.head.appendChild(style);
 };
 
 nextItemBtn.addEventListener('click', nextBtnHandler);
